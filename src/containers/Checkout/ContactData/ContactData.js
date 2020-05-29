1d45f5716ca45b02b6b4fe1ccc3c70af1b54291a
NealDeng77 <nealdeng77@gmail.com>
2020-05-29 11:31:39 -0700

    create input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,8 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
+
 class ContactData extends Component {
     state = {
         name: '',
@@ -48,10 +50,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input type="text" className={classes.Input} name="name" placeholder="Your Name"/>
-                <input type="email" className={classes.Input} name="email" placeholder="Your Email"/>
-                <input type="text" className={classes.Input} name="street" placeholder="Street"/>
-                <input type="text" className={classes.Input} name="postal" placeholder="Postal Code"/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email"/>
+                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
